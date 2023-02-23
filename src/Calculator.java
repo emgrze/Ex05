4c41ffad926c52efa02e7d755b3a6de0446b6c04
Metin <littlesweetream@interia.eu>
2023-02-23 23:52:19 +0100

    NowyPoczatek

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,27 @@
 public class Calculator {
-}
+    public int dodawanie(int firstNumber2, int secondNumber2) {
+        System.out.println("Dodawanie: " + (firstNumber2+secondNumber2));
+        return (firstNumber2 + secondNumber2);
+    }
+    public int odejmowanie(int firstNumber2, int secondNumber2) {
+        System.out.println("Odejmowanie: " + (firstNumber2-secondNumber2));
+        return (firstNumber2 - secondNumber2);
+    }
+    public int mnozenie(int firstNumber2, int secondNumber2) {
+        System.out.println("Mnożenie: " + (firstNumber2*secondNumber2));
+        return (firstNumber2 * secondNumber2);
+    }
+    public int dzielenie(int firstNumber2, int secondNumber2) {
+        int modulo;
+        modulo = firstNumber2%secondNumber2;
+        while(modulo == 0){
+            System.out.println("Dzielenie: " + (firstNumber2/secondNumber2));
+            return (firstNumber2 / secondNumber2);
+        }
+        return (firstNumber2/secondNumber2);
+
+    }
+    public int procent(int firstNumber2, int secondNumber2) {
+        System.out.println("Jaki % liczby1 stanowi liczba2: " + (firstNumber2 / secondNumber2) * 10 + "%");
+        return (firstNumber2 / secondNumber2) * 10;
+    }}
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class CalculatorTest {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Calculator dodawanie = new Calculator();
+            Calculator odejmowanie = new Calculator();
+            Calculator mnozenie = new Calculator();
+            Calculator dzielenie = new Calculator();
+            Calculator procent = new Calculator();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj liczbe 1: ");
+            int firstNumber2 = scanner.nextInt();
+            System.out.println("Podaj liczbe 2: ");
+            int secondNumber2 = scanner.nextInt();
+
+            dodawanie.dodawanie(firstNumber2,secondNumber2);
+            odejmowanie.odejmowanie(firstNumber2,secondNumber2);
+            mnozenie.mnozenie(firstNumber2,secondNumber2);
+            dzielenie.dzielenie(firstNumber2,secondNumber2);
+            procent.procent(firstNumber2,secondNumber2);
+        }
+    }
 }
