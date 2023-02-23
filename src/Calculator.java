16ab66f091c578232c7c052600a1764be400ffed
Metin <littlesweetream@interia.eu>
2023-02-24 00:46:50 +0100

    NowyPoczatekV1.1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,25 @@
 public class Calculator {
-    public int dodawanie(int firstNumber2, int secondNumber2) {
-        System.out.println("Dodawanie: " + (firstNumber2+secondNumber2));
+    public int add(int firstNumber2, int secondNumber2) {
+        System.out.println("Addition: " + (firstNumber2+secondNumber2));
         return (firstNumber2 + secondNumber2);
     }
-    public int odejmowanie(int firstNumber2, int secondNumber2) {
-        System.out.println("Odejmowanie: " + (firstNumber2-secondNumber2));
+    public int sub(int firstNumber2, int secondNumber2) {
+        System.out.println("Subtraction: " + (firstNumber2-secondNumber2));
         return (firstNumber2 - secondNumber2);
     }
-    public int mnozenie(int firstNumber2, int secondNumber2) {
-        System.out.println("Mnożenie: " + (firstNumber2*secondNumber2));
+    public int mul(int firstNumber2, int secondNumber2) {
+        System.out.println("Multiplication: " + (firstNumber2*secondNumber2));
         return (firstNumber2 * secondNumber2);
     }
-    public int dzielenie(int firstNumber2, int secondNumber2) {
+    public int div(int firstNumber2, int secondNumber2) {
         int modulo;
         modulo = firstNumber2%secondNumber2;
         while(modulo == 0){
-            System.out.println("Dzielenie: " + (firstNumber2/secondNumber2));
+            System.out.println("Division: " + (firstNumber2/secondNumber2));
             return (firstNumber2 / secondNumber2);
+        }while (modulo != 0){
+            System.out.println("Modulo != 0, division failed");
+            break;
         }
         return (firstNumber2/secondNumber2);
-
-    }
-    public int procent(int firstNumber2, int secondNumber2) {
-        System.out.println("Jaki % liczby1 stanowi liczba2: " + (firstNumber2 / secondNumber2) * 10 + "%");
-        return (firstNumber2 / secondNumber2) * 10;
     }}
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args){
-            Calculator dodawanie = new Calculator();
-            Calculator odejmowanie = new Calculator();
-            Calculator mnozenie = new Calculator();
-            Calculator dzielenie = new Calculator();
-            Calculator procent = new Calculator();
+            Calculator addiition = new Calculator();
+            Calculator subtraction = new Calculator();
+            Calculator multiplication = new Calculator();
+            Calculator division = new Calculator();
+
 
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Podaj liczbe 1: ");
+            System.out.println("Type number 1: ");
             int firstNumber2 = scanner.nextInt();
-            System.out.println("Podaj liczbe 2: ");
+            System.out.println("Type number 2: ");
             int secondNumber2 = scanner.nextInt();
 
-            dodawanie.dodawanie(firstNumber2,secondNumber2);
-            odejmowanie.odejmowanie(firstNumber2,secondNumber2);
-            mnozenie.mnozenie(firstNumber2,secondNumber2);
-            dzielenie.dzielenie(firstNumber2,secondNumber2);
-            procent.procent(firstNumber2,secondNumber2);
+            addiition.add(firstNumber2,secondNumber2);
+            subtraction.sub(firstNumber2,secondNumber2);
+            multiplication.mul(firstNumber2,secondNumber2);
+            division.div(firstNumber2,secondNumber2);
     }
 }
